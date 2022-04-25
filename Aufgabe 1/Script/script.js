36aea44357f6bd4864fee4bc750808276d5c85e9
Sammy159 <86341509+Sammy159@users.noreply.github.com>
2022-04-25 15:43:14 +0200

    Upload Files

---
+++ Aufgabe 1/Script/script.js
@@ -0,0 +1,13 @@
+//Neue Seiten jeweils in neuen Fenstern öffnen
+function weiterKatSchutz(){
+  window.open(href="katastrophenschutz-responsive.html", '_blank', '');
+  return false;
+}
+function weiterSanidienst(){
+  window.open(href="sanitaetsdienst.html", '_blank', '');
+  return false;
+}
+function weiterEval(){
+  window.open(href="evaluation.html", '_blank', '');
+  return false;
+}
